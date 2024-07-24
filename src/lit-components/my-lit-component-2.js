4cd94f82415e2974c8d9da6ee072048797c8831b
Rogier de Ruijter <r.de.ruijter@vpro.nl>
2024-07-24 17:05:58 +0200

    clean up

--- src/lit-components/my-lit-component-2.js
+++
@@ -1,9 +0,0 @@
-import { LitElement, html } from "lit";
-
-export class MyLitComponent2 extends LitElement {
-  render() {
-    return html` <h1>My Lit Component 2</h1> `;
-  }
-}
-
-customElements.define("my-lit-component-2", MyLitComponent2);
